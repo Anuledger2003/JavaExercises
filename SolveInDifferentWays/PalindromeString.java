75dec40563bece5dcdbdffe5d6ac0823057ac326
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-30 15:53:03 +0530

    Add files via upload

---
+++ SolveInDifferentWays/PalindromeString.java
@@ -0,0 +1,76 @@
+package FrequentlyAskedJavaProgram;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+	
+	public static void main(String[] args) {
+	
+	Scanner sc =  new Scanner(System.in);
+	
+	System.out.println("Enter the String : ");
+	String s = sc.nextLine();
+	
+//	String rev ="";
+	
+//	for( int i = s.length() - 1 ; i >= 0 ; i--) {
+//		rev = rev + s.charAt(i);
+//	}
+//	
+//	if(rev.equals(s)) {
+//		System.out.println("Given String is Palindrome");
+//	}
+//	else {
+//		System.out.println("Given String is not a Palindrome");
+//	}
+	
+	
+//	char[] a = s.toCharArray();
+//	 
+//	for(int i = a.length -1 ; i >= 0 ; i--) {
+//		rev = rev + a[i];
+//	}
+	
+//	char[] a = s.toCharArray();
+//	
+//	int left = 0;
+//	int right = a.length -1;
+//	
+//	while(left < right) {
+//		
+//		char temp = a[left];
+//		a[left] = a[right];
+//		a[right] = temp;
+//		left++ ;
+//		right--;
+//	}
+//	
+//	String st = new String(a);
+	
+	
+//	StringBuffer sb = new StringBuffer(s);
+//	StringBuffer b = sb.reverse();
+	
+//	StringBuilder sb = new StringBuilder(s);
+//	StringBuilder b = sb.reverse();
+
+//	String rev = reverseString(s);
+//	
+//	if(rev.equals(s)) {
+//		System.out.println("Given String is Palindrome");
+//	}
+//	else {
+//		System.out.println("Given String is not a Palindrome");
+//	}
+//	
+//	}
+//	
+//	static String reverseString(String s) {
+//		
+//		if(s.isEmpty()) return s;
+//		
+//		return reverseString(s.substring(1)) + s.charAt(0);
+//		
+//	}
+
+}
