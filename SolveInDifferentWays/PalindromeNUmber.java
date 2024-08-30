6630605e7d9eff7e091d397e4f3b9353cece796c
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-30 13:30:07 +0530

    Add files via upload

---
+++ SolveInDifferentWays/PalindromeNUmber.java
@@ -0,0 +1,48 @@
+package FrequentlyAskedJavaProgram;
+
+import java.util.Scanner;
+
+public class PalindromeNUmber {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the Number : ");
+		int num = sc.nextInt();
+//		int rev = 0;
+//		int temp = num;
+//		while (temp != 0) {
+//
+//			rev = rev * 10 + temp % 10;
+//			temp = temp / 10;
+//		}
+//
+//		System.out.println(rev);
+
+		
+		
+		 
+//        StringBuffer sb = new StringBuffer(String.valueOf(num));
+//        StringBuffer sbrev = sb.reverse();
+//        
+//        String s =sbrev.toString();
+		
+//		StringBuilder sb = new StringBuilder();
+//		StringBuilder sbrev = sb.append(num).reverse();
+//		
+//		String s = sbrev.toString();
+        
+//        System.out.println(s);
+//       
+//        if(Integer.valueOf(s) == num) { 
+//			System.out.println("The Given Number is Palindrome"); 
+//		} 
+//		else
+//		{ 
+//		System.out.println("The Given Number is not Palindrome"); 
+//		}
+		
+	}
+
+}
