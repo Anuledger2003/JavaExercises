f32ff3f5bb7bd87b674f3c6b147e4ae813fbe0b0
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-30 16:04:40 +0530

    Add files via upload

---
+++ SolveInDifferentWays/SumOfNum.java
@@ -0,0 +1,37 @@
+package FrequentlyAskedJavaProgram;
+
+import java.util.Scanner;
+
+public class SumOfNum {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the Number : ");
+		int n = sc.nextInt();
+		int sum = 0;
+		
+//		while(n != 0) {
+//			sum =sum + n%10;
+//			n /=10;
+//		}
+//      System.out.println("Sum of Numbers  : " + sum);	
+		
+//		int sums = SumOfNUM(n);
+//		
+//		
+//		System.out.println("Sum of Numbers  : " + sums);
+//	}
+//	
+//	static int SumOfNUM(int n) {
+//		
+//		if(n == 0) return 0 ;
+//		
+//		return n%10 + SumOfNUM(n/10); 
+//		
+//	
+		}
+
+}
